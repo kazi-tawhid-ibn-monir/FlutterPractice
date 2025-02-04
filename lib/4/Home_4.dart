6892420f1147972b893adf104e6cfe79f012582a
KaziTanvir <kazitawhidibnmonir@gmail.com>
2025-02-05 00:44:25 +0600

    Simple Fform Created that used validator to check parameters

--- lib/4/Home_4.dart
+++ lib/4/Home_4.dart
@@ -5,16 +5,94 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text("Ostad App"),
+        title: Text("Simple Form"),
         centerTitle: true,
       ),
-      body: Column(),
-      floatingActionButton: FloatingActionButton.extended(onPressed: (){
-        print("Button Pressed");
-      },label: Text("Add New"),),
+      body: Column(
+        children: [
+          Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: email,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "E-mail",
+                          label: Text("Email")),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Empty Email not allowed";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: password,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Password",
+                          label: Text("Password")),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Empty Password Not Allowed";
+                        } else if (value.length < 6) {
+                          return "Password must be 6 charechter";
+                        }
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {}
+                          },
+                          child: Text(
+                            "Submit",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange),
+                          onPressed: () {
+                            email.clear();
+                            password.clear();
+                          },
+                          child: Text(
+                            "Clear",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          )),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          print("Button Pressed");
+        },
+        label: Text("Add New"),
+      ),
     );
   }
 }
